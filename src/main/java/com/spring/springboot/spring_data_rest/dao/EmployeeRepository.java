6ea0d7a33e5d1bfe34b974ee16935f3e92aa29db
noleynik29 <noleynik29@gmail.com>
2022-05-23 13:41:16 +0300

    Java Spring Boot Data REST App using: spring-boot-starter-data-jpa, spring-boot-starter-web, spring-boot-starter-data-rest, spring-boot-starter-actuator, mysql-connector-java, spring-boot-starter-test and spring-boot-starter-security.

---
+++ src/main/java/com/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.spring.springboot.spring_data_rest.dao;
+
+import com.spring.springboot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
